d529309915f241947a7fb76909c11a2867f4d22f
AdiKub <adiletkub@gmail.com>
2019-12-05 12:01:42 +0600

    added other lights, sky and shadow

--- js/main.js
+++ js/main.js
@@ -2,48 +2,71 @@ var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
 
 var renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
+
 scene.background = new THREE.Color( 0x000000 );
 scene.fog = new THREE.Fog( 0x000000, 250, 1400 );
-window.onresize = () => renderer.setSize( window.innerWidth, window.innerHeight );
-document.body.appendChild( renderer.domElement );
 
 // CAMERA
 camera = new THREE.PerspectiveCamera( 30, window.innerWidth / window.innerHeight, 1, 1500 );
-var controls = new THREE.OrbitControls( camera, renderer.domElement );
-var cameraTarget = new THREE.Vector3( 0, 0, 0 );
-camera.position.set( 0, 2, 8 );
-controls.update();
+//var controls = new THREE.OrbitControls( camera, renderer.domElement );
+var cameraTarget = new THREE.Vector3( 0, 1, 5 );
+camera.position.set( 0, 2, 10 );
+//controls.update();
 
 // SCENE
 scene = new THREE.Scene();
-scene.background = new THREE.Color( 0x000000 );
-scene.fog = new THREE.Fog( 'blue', 0, 20);
+scene.background = new THREE.Color().setHSL( 0.6, 0, 1 );
+scene.fog = new THREE.Fog( scene.background, 1, 5000 );
+scene.fog = new THREE.Fog( 'white', -10, 70);
+
+// Lights
+var hemiLight = new THREE.HemisphereLight(  0xffffff, 0xffffff, 0.6 );
+hemiLight.color.setHSL( 0.6, 1, 0.6 );
+hemiLight.groundColor.setHSL( 0.095, 1, 0.75 );
+hemiLight.position.set( 0, 20, 0 );
+scene.add( hemiLight );
+
+dirLight = new THREE.DirectionalLight( 0xffffff, 1 );
+dirLight.color.setHSL( 0.1, 1, 0.95 );
+dirLight.position.set( - 1, 1.75, 1 );
+dirLight.position.multiplyScalar( 30 );
+dirLight.castShadow = true;
+scene.add( dirLight );
+
+dirLightHeper = new THREE.DirectionalLightHelper( dirLight, 10 );
+scene.add( dirLightHeper );
+
 
 var boxGeometry = new THREE.BoxGeometry( 1, 1, 1 );
 var boxMaterial = new THREE.MeshStandardMaterial({
   color: 'red',
   metalness: 0,
 });
-
-var planeGeometry = new THREE.PlaneGeometry(2, 2, 2);
-var planeMaterial = new THREE.MeshStandardMaterial({
-  color: 'black',
-  metalness: 3,
-});
-
 var cube = new THREE.Mesh( boxGeometry, boxMaterial );
-var plane = new THREE.Mesh( planeGeometry, planeMaterial );
-plane.rotation.x = 1;
+cube.castShadow = true;
+cube.receiveShadow = true;
+cube.position.y = 1
 cube.scale = 2;
 scene.add( cube );
-scene.add( plane );
-scene.add( new THREE.HemisphereLight( 0xaaaaaa, 0x444444 ) );
+
+var groundGeo = new THREE.PlaneBufferGeometry( 10000, 10000 );
+var groundMat = new THREE.MeshLambertMaterial( { color: 0xffffff } );
+groundMat.color.setHSL( 0.095, 1, 0.75 );
+var ground = new THREE.Mesh( groundGeo, groundMat );
+ground.position.y = 0;
+ground.rotation.x = - Math.PI / 2;
+ground.receiveShadow = true;
+ground.castShadow = true;
+scene.add( ground );
+
+renderer.setSize( window.innerWidth, window.innerHeight );
+document.body.appendChild( renderer.domElement );
+renderer.shadowMap.enabled = true;
 
 var animate = function () {
   requestAnimationFrame( animate );
-  controls.update();
-
+  //controls.update();
+  
   cube.rotation.y += 0.01;
   renderer.render(
     scene,
