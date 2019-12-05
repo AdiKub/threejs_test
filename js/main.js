0470d2da30c79f73e3838e73562688b1247aa610
AdiKub <adiletkub@gmail.com>
2019-12-05 18:12:36 +0600

    added animation

--- js/main.js
+++ js/main.js
@@ -4,23 +4,20 @@ var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
 
 var renderer = new THREE.WebGLRenderer();
-
-scene.background = new THREE.Color( 0x000000 );
-scene.fog = new THREE.Fog( 0x000000, 250, 1400 );
+var backColor = new THREE.Color('#c5dff4')
 
 // CAMERA
 camera = new THREE.PerspectiveCamera( 30, window.innerWidth / window.innerHeight, 1, 1500 );
 var cameraTarget = new THREE.Vector3( 0, 1, 0 );
-camera.position.set( 0, 2, 10 );
+camera.position.set( 0, 2, -10 );
 
 var controls = new THREE.OrbitControls( camera, renderer.domElement );
 controls.update();
 
 // SCENE
 scene = new THREE.Scene();
-scene.background = new THREE.Color().setHSL( 0.6, 0, 1 );
-scene.fog = new THREE.Fog( scene.background, 1, 5000 );
-scene.fog = new THREE.Fog( 'white', -10, 70);
+scene.background = backColor;
+scene.fog = new THREE.Fog( backColor, -10, 70);
 
 // Lights
 var hemiLight = new THREE.HemisphereLight(  0xffffff, 0xffffff, 0.6 );
@@ -40,17 +37,17 @@ dirLightHeper = new THREE.DirectionalLightHelper( dirLight, 10 );
 scene.add( dirLightHeper );
 
 
-var boxGeometry = new THREE.BoxGeometry( 1, 1, 1 );
+var spherGeometry = new THREE.SphereGeometry( 0.2, 20, 30 );
 var boxMaterial = new THREE.MeshStandardMaterial({
   color: 'red',
   metalness: 0,
 });
-var cube = new THREE.Mesh( boxGeometry, boxMaterial );
-cube.castShadow = true;
-cube.receiveShadow = true;
-cube.position.y = 1
-cube.scale = 2;
-scene.add( cube );
+
+var sphere = new THREE.Mesh( spherGeometry, boxMaterial );
+sphere.castShadow = true;
+sphere.receiveShadow = true;
+
+scene.add( sphere );
 
 var groundGeo = new THREE.PlaneBufferGeometry( 10000, 10000 );
 var groundMat = new THREE.MeshLambertMaterial( { color: 0xffffff } );
@@ -66,13 +63,27 @@ renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 renderer.shadowMap.enabled = true;
 
+let pos = 2;
+let switcher = true;
+
+const bound = () => {
+  if (switcher) {
+    if (pos < 2){switcher = false}
+    return pos /= 2
+  } else {
+    if (pos > 400){switcher = true}
+    return pos *= 2
+  };
+}
+
 var animate = function () {
+
   controls.update();
-  
-  
-  
   requestAnimationFrame( animate );
-  cube.rotation.y += 0.01;
+  
+  
+  sphere.position.y = bound()/100;
+
   renderer.render(
     scene,
     camera,
